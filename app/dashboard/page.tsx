4c9916f4efa4767a9fb5bd4e422b14dcce374fc6
nejcc <nejc.cotic@gmail.com>
2024-08-21 00:23:09 +0200

    connect with auth kinde

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,29 +1,15 @@
+
 import { Button } from "@/components/ui/button";
 import { RegisterLink, LoginLink, LogoutLink } from "@kinde-oss/kinde-auth-nextjs/components";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 
-export default async function Home() {
+export default async function Dashboard() {
   const { getUser } = getKindeServerSession();
   const session = await getUser();
-  
   return (
     <div className="p-5">
       Welcome
-
-      {session ? (
-          <LogoutLink>
-            <Button>Logout</Button>
-          </LogoutLink>
-      ): (
-          <div>
-            <LoginLink>
-        <Button>Login</Button>
-      </LoginLink >
-    <RegisterLink>
-      <Button>Register now</Button>
-    </RegisterLink>
-          </div>
-        )}
-    </div>
+Hellow dashboard
+    </div >
   );
 }
